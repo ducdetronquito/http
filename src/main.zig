721b24fd6cc874b63d56d183b2f78e9539b7c58b
ducdetronquito <g.paulet@zero-gachis.com>
2020-09-21 23:01:49 +0200

    :sparkles: Request - Expose the RequestError error type.

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,6 @@ pub const HeaderMap = @import("headers.zig").HeaderMap;
 pub const Method = @import("methods.zig").Method;
 pub const Request = @import("request.zig").Request;
 pub const RequestBuilder = @import("request.zig").RequestBuilder;
+pub const RequestError = @import("request.zig").RequestError;
 pub const Uri = @import("uri.zig").Uri;
 pub const Version = @import("versions.zig").Version;
