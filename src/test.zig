d6d4b556e36775af560e8e279805698ca1858d93
ducdetronquito <g.paulet@zero-gachis.com>
2020-10-06 21:55:33 +0200

    :sparkles: Expose StatusCode and StatusCodeError in the public API.

--- src/test.zig
+++ src/test.zig
@@ -2,5 +2,6 @@ usingnamespace @import("headers.zig");
 usingnamespace @import("methods.zig");
 usingnamespace @import("request.zig");
 usingnamespace @import("response.zig");
+usingnamespace @import("status.zig");
 usingnamespace @import("uri/uri.zig");
 usingnamespace @import("versions.zig");
