803440bb92871dd611405dc0bfb347cd23a8af79
ducdetronquito <g.paulet@zero-gachis.com>
2020-11-02 12:35:20 +0100

    :sparkles: Method - Add from_bytes function.

--- src/methods.zig
+++ src/methods.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const MethodType = enum {
     Connect,
     Custom,
@@ -37,10 +39,61 @@ pub const Method = union(MethodType) {
             .Trace => "TRACE",
         };
     }
+
+    pub fn from_bytes(value: []const u8) Method {
+        switch(value.len) {
+            3 => {
+                if (std.mem.eql(u8, value, "GET")) {
+                    return .Get;
+                } else if (std.mem.eql(u8, value, "PUT")) {
+                    return .Put;
+                }
+                else {
+                    return Method { .Custom = value };
+                }
+            },
+            4 => {
+                if (std.mem.eql(u8, value, "HEAD")) {
+                    return .Head;
+                } else if (std.mem.eql(u8, value, "POST")) {
+                    return .Post;
+                } else {
+                    return Method { .Custom = value };
+                }
+            },
+            5 => {
+                if (std.mem.eql(u8, value, "PATCH")) {
+                    return .Patch;
+                } else if (std.mem.eql(u8, value, "TRACE")) {
+                    return .Trace;
+                } else {
+                    return Method { .Custom = value };
+                }
+            },
+            6 => {
+                if (std.mem.eql(u8, value, "DELETE")) {
+                    return .Delete;
+                } else {
+                    return Method { .Custom = value };
+                }
+            },
+            7 => {
+                if (std.mem.eql(u8, value, "CONNECT")) {
+                    return .Connect;
+                } else if (std.mem.eql(u8, value, "OPTIONS")) {
+                    return .Options;
+                } else {
+                    return Method { .Custom = value };
+                }
+            },
+            else => {
+                return Method { .Custom = value };
+            }
+        }
+    }
 };
 
 const expect = std.testing.expect;
-const std = @import("std");
 
 test "Convert to bytes" {
     var connect = Method { .Connect = undefined };
@@ -72,4 +125,21 @@ test "Convert to bytes" {
 
     var trace = Method { .Trace = undefined };
     expect(std.mem.eql(u8, trace.to_bytes(), "TRACE"));
+}
+
+test "FromBytes - Success" {
+    expect(Method.from_bytes("CONNECT") == .Connect);
+    expect(Method.from_bytes("DELETE") == .Delete);
+    expect(Method.from_bytes("GET") == .Get);
+    expect(Method.from_bytes("HEAD") == .Head);
+    expect(Method.from_bytes("OPTIONS") == .Options);
+    expect(Method.from_bytes("PATCH") == .Patch);
+    expect(Method.from_bytes("POST") == .Post);
+    expect(Method.from_bytes("PUT") == .Put);
+    expect(Method.from_bytes("TRACE") == .Trace);
+
+    switch (Method.from_bytes("LAUNCH-MISSILE")) {
+        .Custom => |name| expect(std.mem.eql(u8, name, "LAUNCH-MISSILE")),
+        else => unreachable,
+    }
 }
